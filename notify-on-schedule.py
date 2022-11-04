b91d558affdf639aded7ebd6c9c085fa3f1a4ec6
Misaka-Aki <66006338+Misaka-Aki@users.noreply.github.com>
2022-11-04 10:37:12 +0800

    Update notify-on-schedule.py

--- notify-on-schedule.py
+++ notify-on-schedule.py
@@ -2,7 +2,7 @@ import os
 
 def notify():
     from onepush import notify
-    notify('serverchanturbo', sctkey=os.environ['KEY'], title='OnePush', content='Hello World!')
+    notify('serverchanturbo', sctkey=os.environ['KEY'], title='核酸检测提醒', content='核酸结果已三天')
     
 if __name__ == '__main__':
     notify()
